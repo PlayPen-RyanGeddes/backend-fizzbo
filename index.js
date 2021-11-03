486797dbddcee8cef385053495699ca76750a7ef
RYAN GEDDES <rgeddes09@gmail.com>
2021-11-02 21:40:55 -0700

    deploying test listings route and schema

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 let mongoose = require('mongoose');
 let server = require('./server.js');
 
+//MONGODB_URI=mongodb://localhost:27017/fizzbo-test
+
 mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
